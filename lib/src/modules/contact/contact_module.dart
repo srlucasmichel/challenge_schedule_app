d12e7761590e55dd7b837885354f17e9f2cf3ff1
Lucas Michel <80552835+srlucasmichel@users.noreply.github.com>
2022-11-19 17:21:48 -0500

    module changed

--- lib/src/modules/contact/contact_module.dart
+++ lib/src/modules/contact/contact_module.dart
@@ -1,8 +1,15 @@
+import 'package:challenge_schedule_app/src/modules/contact/presenter/stores/contact_store.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:uno/uno.dart';
 
 import 'domain/repositories/user_repository.dart';
+import 'domain/usecases/change_user.dart';
+import 'domain/usecases/create_user.dart';
+import 'domain/usecases/delete_user.dart';
+import 'domain/usecases/get_users.dart';
+import 'external/datasources/users_datasource.dart';
 import 'infra/data/sources/local/daos/user_dao.dart';
+import 'infra/datasources/user_datasource.dart';
 import 'presenter/pages/contact_detail_page.dart';
 import 'presenter/pages/contact_form_page.dart';
 import 'presenter/pages/contact_list_page.dart';
@@ -13,13 +20,16 @@ class ContactModule extends Module {
         //utils
         Bind.factory((i) => Uno()),
         //datasource
-
+        Bind.factory<IUserDatasource>((i) => UsersDatasource(i())),
         //repository
         Bind.factory<IUserRepository>((i) => UserRepository(i())),
-
         //usecase
-
+        Bind.factory((i) => ChangeUser(i())),
+        Bind.factory((i) => CreateUser(i())),
+        Bind.factory((i) => DeleteUser(i())),
+        Bind.factory((i) => GetUsers(i())),
         //store
+        Bind.singleton((i) => ContactStore(i())),
       ];
 
   @override
